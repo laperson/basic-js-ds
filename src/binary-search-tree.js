fd7df48e92ddd7e58d39a07dd07e5f9b32ee3eca
laperson <43199823+laperson@users.noreply.github.com>
2023-04-17 23:14:24 +0300

    feat: done 1 task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,43 +6,157 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
   }
 }
+
+class BinarySearchTree {
+
+  constructor() {
+    this.rootN = null
+  }
+
+  root() {
+    return this.rootN;
+  }
+
+  add(data) {
+    let node = new Node(data);
+    if (this.rootN == null) {
+      this.rootN = node;
+      return;
+    }
+
+    let currN = this.rootN;
+    while (currN !== null) {
+      if (data < currN.data) {
+
+        if (currN.left == null) {
+         currN.left = node;
+          break;
+        } else currN = currN.left;
+
+      } else if (data > currN.data) {
+
+        if (currN.right == null) {
+          currN.right = node;
+        break;
+
+        } else currN = currN.right;
+      } else break;
+    }}
+
+  has(data) {
+    let currN = this.rootN;
+
+    while (currN !== null) {
+      if (data < currN.data) {
+        currN = currN.left;
+      } else if (data > currN.data) {
+        currN = currN.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  find(data) {
+    let currN = this.rootN;
+
+    while (currN !== null) {
+      if (data < currN.data) {
+        currN = currN.left;
+      } else if (data > currN.data) {
+        currN = currN.right;
+      } else return currN;
+    }
+
+     return null;
+  }
+
+  remove(data) {
+    let parentN = null;
+    let currN = this.rootN;
+
+    while (currN !== null && data !== currN.data) {
+      parentN = currN;
+
+      if (data < currN.data) {
+        currN = currN.left;
+      } else if (data > currN.data) {
+        currN = currN.right;
+      }
+    }
+     if (currN == null) return;
+
+    if (currN.left == null && currN.right == null) {
+
+      if (parentN == null) {
+        this.rootN = null;
+      } else if (parentN.left == currN) {
+        parentN.left = null;
+      } else if (parentN.right == currN) {
+        parentN.right = null;
+      }
+
+    } else if ((currN.left !== null && currN.right == null) 
+            || (currN.left == null && currN.right !== null)) {
+      let childN = (currN.left !== null && currN.right == null) ? currN.left 
+                                                                 : currN.right;
+      if (parentN == null) {
+        this.rootN = childN;
+      } else if (parentN.left == currN) {
+        parentN.left = childN;
+      } else if (parentN.right == currN) {
+        parentN.right = childN;
+      }
+
+    } else if ((currN.left !== null && currN.right !== null)) {
+       let minRightN = this.findMin(currN.right);
+      let minRightD = minRightN.data;
+      this.remove(minRightD);
+      minRightN.left = currN.left;
+       minRightN.right = currN.right;
+
+      if (parentN == null) {
+        this.rootN = minRightN;
+      } else {
+        if (parentN.left !== null && parentN.left.data == data) {
+          parentN.left = minRightN;
+        } else parentN.right = minRightN;
+      }
+    }}
+
+  findMin(node) {
+    let LeftChild = node;
+
+    while ( LeftChild !== null && LeftChild.left !== null)  LeftChild = LeftChild.left;
+    return LeftChild;
+}
+
+  min() { if (this.rootN == null) return null;
+  let currN = this.rootN;
+  
+  while (currN.left !== null) currN = currN.left;
+  return currN.data;
+  }
+
+  max() { if (this.rootN == null) return null; 
+  let currN = this.rootN;
+  
+  while (currN.right !== null) currN = currN.right;
+  return currN.data;
+  } 
+}
+  
+
+
 
 module.exports = {
   BinarySearchTree
